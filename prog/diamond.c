bb135674ef83b258636ff0cdc2b5895c028b506c
rehan <phirrehan@gmail.com>
2025-02-12 11:25:27 +0500

    added comment

--- prog/diamond.c
+++ prog/diamond.c
@@ -14,20 +14,20 @@ int main() {
   // Writing upper half of diamond.
   for (i = 0; i <= length; i += 2) {
     for (j = center; j > 1 + i / 2; j--) {
-      printf(" ");  // Writing spaces
+      printf(" "); // Writing spaces
     }
     for (j = 0; j <= i; j++) {
-      printf("*");  // Writing asterisks.
+      printf("*"); // Writing asterisks.
     }
     printf("\n");
   }
   // Writing bottom half of diamond.
   for (i = length - 1; i > 1; i -= 2) {
     for (j = center; j > i / 2; j--) {
-      printf(" ");  // Writing spaces
+      printf(" "); // Writing spaces
     }
     for (j = 1; j < i; j++) {
-      printf("*");  // Writing asterisks.
+      printf("*"); // Writing asterisks.
     }
     printf("\n");
   }
